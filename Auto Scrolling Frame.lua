72d87d015195dbd27f21b84667c4f7fa132d9fe8
U_M9 <gavinsharpprivate@gmail.com>
2024-10-31 20:28:08 +1100

    Update Auto Scrolling Frame.lua
    changed from snake_case to camelCase

--- Auto Scrolling Frame.lua
+++ Auto Scrolling Frame.lua
@@ -1,35 +1,37 @@
-local auto_scroll_frame = {}
-auto_scroll_frame.__index = auto_scroll_frame
+local AutoScrollingFrame = {}
+AutoScrollingFrame.__index = AutoScrollingFrame
 
-function auto_scroll_frame.new(frame)
-	local self = setmetatable({}, auto_scroll_frame)
+function AutoScrollingFrame.new(frame)
+	local self = setmetatable({}, AutoScrollingFrame)
 	self.frame = frame
-	self.scrolling_enabled = true
-	self.last_scroll_position = 0
+	self.scrollingEnabled = true
+	self.lastScrollPosition = 0
+	
 	self.frame.UIListLayout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
-		if self.scrolling_enabled then
-			self:scroll_to_bottom()
+		if self.scrollingEnabled then
+			self:scrollToBottom()
 		end
 	end)
+
 	self.frame.CanvasPosition = Vector2.new(0, 0)
 	return self
 end
 
-function auto_scroll_frame:scroll_to_bottom()
+function AutoScrollingFrame:scrollToBottom()
 	self.frame.CanvasPosition = Vector2.new(0, self.frame.UIListLayout.AbsoluteContentSize.Y)
 end
 
-function auto_scroll_frame:set_scrolling_enabled(enabled)
-	self.scrolling_enabled = enabled
+function AutoScrollingFrame:setScrollingEnabled(enabled)
+	self.scrollingEnabled = enabled
 end
 
-function auto_scroll_frame:update()
-	if self.frame.CanvasPosition.Y < self.last_scroll_position then
-		self:set_scrolling_enabled(false)
-	elseif self.frame.CanvasPosition.Y > self.last_scroll_position then
-		self:set_scrolling_enabled(true)
+function AutoScrollingFrame:update()
+	if self.frame.CanvasPosition.Y < self.lastScrollPosition then
+		self:setScrollingEnabled(false)
+	elseif self.frame.CanvasPosition.Y > self.lastScrollPosition then
+		self:setScrollingEnabled(true)
 	end
-	self.last_scroll_position = self.frame.CanvasPosition.Y
+	self.lastScrollPosition = self.frame.CanvasPosition.Y
 end
 
-return auto_scroll_frame
+return AutoScrollingFrame
