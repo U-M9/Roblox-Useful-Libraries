4cc0262124aeba70de752c0a571ac5890ef667e5
U_M9 <gavinsharpprivate@gmail.com>
2024-12-24 21:23:05 +1100

    Update Auto Scrolling Frame.lua
    type definitions

--- Auto Scrolling Frame.lua
+++ Auto Scrolling Frame.lua
@@ -1,37 +1,47 @@
+type AutoScrollingFrame = {
+    frame: ScrollingFrame,
+    scrollingEnabled: boolean,
+    lastScrollPosition: number,
+    new: (frame: ScrollingFrame) -> AutoScrollingFrame,
+    scrollToBottom: (self: AutoScrollingFrame) -> (),
+    setScrollingEnabled: (self: AutoScrollingFrame, enabled: boolean) -> (),
+    update: (self: AutoScrollingFrame) -> ()
+}
+
 local AutoScrollingFrame = {}
 AutoScrollingFrame.__index = AutoScrollingFrame
 
-function AutoScrollingFrame.new(frame)
-	local self = setmetatable({}, AutoScrollingFrame)
-	self.frame = frame
-	self.scrollingEnabled = true
-	self.lastScrollPosition = 0
-	
-	self.frame.UIListLayout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
-		if self.scrollingEnabled then
-			self:scrollToBottom()
-		end
-	end)
+function AutoScrollingFrame.new(frame: ScrollingFrame): AutoScrollingFrame
+    local self = setmetatable({}, AutoScrollingFrame)
+    self.frame = frame
+    self.scrollingEnabled = true
+    self.lastScrollPosition = 0
 
-	self.frame.CanvasPosition = Vector2.new(0, 0)
-	return self
+    self.frame.UIListLayout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
+        if self.scrollingEnabled then
+            self:scrollToBottom()
+        end
+    end)
+
+    self.frame.CanvasPosition = Vector2.new(0, 0)
+    return self
 end
 
 function AutoScrollingFrame:scrollToBottom()
-	self.frame.CanvasPosition = Vector2.new(0, self.frame.UIListLayout.AbsoluteContentSize.Y)
+    self.frame.CanvasPosition = Vector2.new(0, self.frame.UIListLayout.AbsoluteContentSize.Y)
 end
 
-function AutoScrollingFrame:setScrollingEnabled(enabled)
-	self.scrollingEnabled = enabled
+function AutoScrollingFrame:setScrollingEnabled(enabled: boolean)
+    self.scrollingEnabled = enabled
 end
 
 function AutoScrollingFrame:update()
-	if self.frame.CanvasPosition.Y < self.lastScrollPosition then
-		self:setScrollingEnabled(false)
-	elseif self.frame.CanvasPosition.Y > self.lastScrollPosition then
-		self:setScrollingEnabled(true)
-	end
-	self.lastScrollPosition = self.frame.CanvasPosition.Y
+    if self.frame.CanvasPosition.Y < self.lastScrollPosition then
+        self:setScrollingEnabled(false)
+    elseif self.frame.CanvasPosition.Y > self.lastScrollPosition then
+        self:setScrollingEnabled(true)
+    end
+    self.lastScrollPosition = self.frame.CanvasPosition.Y
 end
 
 return AutoScrollingFrame
